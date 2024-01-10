553b066571f7b42ac1ba0254558bcdaa4cc9ba96
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2024-01-10 09:16:50 +0530

    Create meal item component

---
+++ src/components/MealItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white shadow-md rounded-xl">
+    <img
+      :src="meal.strMealThumb"
+      :alt="meal.strMeal"
+      class="rounded-t-xl h-48 w-full object-cover"
+    />
+
+    <div class="p-3">
+      <h3 class="font-semibold">{{ meal.strMeal }}</h3>
+      <div class="mt-4 flex gap-4 items-center">
+        <a
+          :href="meal.strYoutube"
+          target="_blank"
+          class="px-3 py-2 text-xs rounded border-2 border-red-500 bg-red-500 text-white hover:bg-red-600 hover:border-red-600 transition-all"
+          >YouTube</a
+        >
+        <router-link
+          :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
+          class="px-3 py-2 text-xs rounded border-2 hover:bg-gray-100 transition-all"
+        >
+          View
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const { meal } = defineProps({
+  meal: Object,
+});
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -10,38 +10,11 @@
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="bg-white shadow-md rounded-xl"
-    >
-      <img
-        :src="meal.strMealThumb"
-        :alt="meal.strMeal"
-        class="rounded-t-xl h-48 w-full object-cover"
-      />
-
-      <div class="p-3">
-        <h3 class="font-semibold">{{ meal.strMeal }}</h3>
-        <div class="mt-4 flex gap-4 items-center">
-          <a
-            :href="meal.strYoutube"
-            target="_blank"
-            class="px-3 py-2 text-xs rounded border-2 border-red-500 bg-red-500 text-white hover:bg-red-600 hover:border-red-600 transition-all"
-            >YouTube</a
-          >
-          <router-link
-            :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
-            class="px-3 py-2 text-xs rounded border-2 hover:bg-gray-100 transition-all"
-          >
-            View
-          </router-link>
-        </div>
-      </div>
-    </div>
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 <script setup>
+import MealItem from "../components/MealItem.vue";
 import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
